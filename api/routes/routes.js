4f3c15dfc77200b3339798b734239f7178c91a72
Vidushan <kvidushan@hotmail.com>
2021-12-21 15:29:59 -0800

    feat: ability to search movie by name

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,7 +4,18 @@ const knex = require("knex")(knexConfig);
 
 // Output all movies
 router.get('/api/movie/all', (req, res) => {
-	knex.select('title', 'description', 'year').from('movie').then((movie) => res.json(movie))
+	knex.select('title', 'description', 'year')
+	.from('movie')
+	.then((movie) => res.json(movie))
+})
+
+// Output movies by name
+router.get('/api/movie/:id', (req, res) => {
+	knex.select()
+	.from('movie')
+	.where({ title: req.params.id })
+	.then((movie) => res.json(movie))
+	.catch((err) => res.status(500).jsonp({ status: 500, message: err.message }))
 })
 
 module.exports = router;
